6a5d712f3c3f59e076ec715864f9e629c3693763
Amit Chopra <amit.chopra@rsystems.com>
2024-01-30 18:44:14 +0530

    pagenotfound

--- lib/routes/screen/page_not_foud.dart
+++ lib/routes/screen/page_not_foud.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shoal_app/routes/app_route.dart';
+import 'package:shoal_app/routes/route_utils.dart';
 
-class NotFoundScreen extends StatelessWidget {
+class NotFoundScreen extends StatefulWidget {
   const NotFoundScreen({Key? key}) : super(key: key);
 
   @override
+  State<NotFoundScreen> createState() => _NotFoundScreenState();
+}
+
+class _NotFoundScreenState extends State<NotFoundScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'No Page Found'
+    void submit () {
+      AppRouter.router.go(PAGES.login.screenPath);
+    }
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page not Found!')),
+      body: Center(
+        child: Column(
+          children: [
+           const SizedBox(height: 50.0),  
+           const  Text(
+            'No Page Found'
+          ),
+          const SizedBox(height: 20.0),
+          TextButton(
+            onPressed: submit,
+             child: const Text('Back to Login!'))
+          ],
+        ),
       ),
     );
   }
